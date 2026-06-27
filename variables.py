2bc493ca03451eeddde6a17ab5982014483633b9
teja31575 <2400031575@kluniversity.in>
2026-06-27 18:39:01 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,115 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(3+8)
+11
+a=10
+print(a)
+10
+b=20
+print(b)
+20
+print(x)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+x=40
+print(x)
+40
+z=50
+print(z)
+50
+a3=80
+print(a3)
+80
+6a=90
+SyntaxError: invalid decimal literal
+b123456=300
+print(b123456)
+300
+@=60
+SyntaxError: invalid syntax
+_=50
+print(_)
+50
+_=90
+print(_)
+90
+_=100
+print(_)
+100
+if=20
+SyntaxError: invalid syntax
+a=4,b=9
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=4;b=9
+print(a+b)
+13
+a,b=3,4
+print(a,b)
+3 4
+a=2,3,4
+print(a)]
+SyntaxError: unmatched ']'
+a=2,3,4
+print(a)
+(2, 3, 4)
+a,b,c=10
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    a,b,c=10
+TypeError: cannot unpack non-iterable int object
+a=b=c=10
+print(a,b,c)
+10 10 10
+a,b,c=(2,3,4)
+print(a,b,c)
+2 3 4
+a,b,c=2,3,4,5,6,7,8,9
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    a,b,c=2,3,4,5,6,7,8,9
+ValueError: too many values to unpack (expected 3, got 8)
+first name="teja"
+SyntaxError: invalid syntax
+first_name="teja"
+>>> print(first_name)
+teja
+>>> firstname="teja"
+>>> print(firstname)
+teja
+>>> fname="teja"
+>>> lname="ch"
+>>> print(fname+lname)
+tejach
+>>> print(fname+" "+lname)
+teja ch
+>>> print(fname,lname)
+teja ch
+>>> name="teja"
+>>> print(name)
+teja
+>>> print("name")
+name
+>>> city="vja"
+>>> print(city)
+vja
+>>> a=5
+>>> print(a)
+5
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#57>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a3'?
+>>> name="teja"
+>>> print(name)
+teja
+>>> Name="teja"
+>>> print(Name)
+teja
+>>> NAME="teja"
+>>> print(NAME)
+teja
